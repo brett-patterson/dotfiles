dbe5fd1772ff8d2def3b665e799b59ae79beb753
Brett Patterson <brett@titan.com>
2023-02-17 18:11:48 -0500

    Update for warp

--- .zshrc
+++ .zshrc
@@ -9,21 +9,12 @@ fi
 
 zplug load
 
-update_terminal_title() {
-    title=$(basename $PWD)
-    echo -ne "\e]1;$title\a"
-}
-
-precmd_functions=($precmd_functions update_terminal_title)
+. "$(brew --prefix asdf)/libexec/asdf.sh"
 
 alias g="git"
 alias gs="g status"
 
 export PATH=".:$HOME/bin:$PATH"
-
-export ATUIN_NOBIND="true"
-eval "$(atuin init zsh)"
-bindkey '^r' _atuin_search_widget
 
 mkdir -p ~/.zshrc.d
 for f in $(ls $HOME/.zshrc.d/); do
--- install.sh
+++ install.sh
@@ -8,26 +8,8 @@ link() {
 }
 
 brew install zplug
-brew install atuin
+brew install asdf
 
 link .zsh_plugins
 link .zshrc
 link bin
-
-REPO_BASE="$HOME/.zsh-repos"
-
-clone_or_update_repo() {
-	repo_url="$1"
-	repo_name="$2"
-
-	repo_path="$REPO_BASE/$repo_name"
-	if [ -d $repo_path ]; then
-		git -C $repo_path pull
-	else
-		mkdir -p $repo_path
-		git clone $repo_url $repo_path
-	fi
-}
-
-clone_or_update_repo https://github.com/dracula/iterm.git dracula/iterm
-open ~/.zsh-repos/dracula/iterm/Dracula.itermcolors
